7dc16253086eb11bad4849ea707170743890f48c
RickJ9 <1421129695@qq.com>
2024-01-21 02:01:35 +0800

    地址簿（添加，查询，修改，删除，设置默认地址）功能代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
@@ -9,6 +10,13 @@ import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapper {
+    /**
+     * 根据用户ID删除购物车数据
+     * @param userId
+     */
+    @Delete("delete from shopping_cart where user_id=#{userId}")
+    void deleteByUserId(Long userId);
+
     /**
      * 动态条件查询
      * @param shoppingCart
